553e71ddb02e97ee5c79c552b634fe1fbcb79591
Deondre2002 <dj232011@gmail.com>
2025-10-07 19:09:08 -0400

    Bookdetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Bookdetail from "./Bookdetail";
+import "./index.css";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="center">
+              <h1>Welcome to Book Buddy!</h1>
+            </div>
+          }
+        />
+        <Route path="/books/:id" element={<Bookdetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
