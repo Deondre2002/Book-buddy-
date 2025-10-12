c63543b592e3555735699617706e6d02c6aa65f3
Deondre2002 <dj232011@gmail.com>
2025-10-11 21:06:10 -0400

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Bookdetail from "./Bookdetail";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
+import Bookdetail from "./Bookdetail";
+import AccountReservation from "./AccountReservations";
+import Portrait from "./Portrait";
 
 export default function App() {
+  const token = "";
+
   return (
     <BrowserRouter>
+      <nav style={{ textAlign: "center", margin: "20px" }}>
+        <Link to="/">Home</Link> | <Link to="/books">Books</Link> |{" "}
+        <Link to="/account">My Account</Link>
+      </nav>
+
       <Routes>
         <Route
           path="/"
           element={
-            <div className="center">
+            <div style={{ textAlign: "center" }}>
               <h1>Welcome to Book Buddy!</h1>
+              <Portrait />
             </div>
           }
         />
-        <Route path="/books/:id" element={<Bookdetail />} />
+
+        <Route path="/account" element={<AccountReservation token={token} />} />
+
+        <Route path="/books" element={<Bookdetail token={token} />} />
+        <Route path="/books/:id" element={<Bookdetail token={token} />} />
       </Routes>
     </BrowserRouter>
   );
